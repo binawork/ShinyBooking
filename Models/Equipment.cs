a818fd344b0df3bbe606ededd5df0da3d48b1281
zalewskiwojciech <xiehu43@gmail.com>
2020-03-11 14:28:39 +0100

    Equipment and Room Equipment added

---
+++ Models/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShinyBooking.Models
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Models/RoomsEquipments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShinyBooking.Models
+{
+    public class RoomsEquipments
+    {
+        public int EquipmentId { get; set; }
+
+        public Equipment Equipment { get; set; }
+
+        public int RoomId { get; set; }
+
+        public Room Room { get; set; }
+    }
+}
