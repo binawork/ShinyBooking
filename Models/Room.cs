056d713c821ccc9195241b344ba1d2f423d57bd4
Bogdan Bańkowski <Hevixxx@gmail.com>
2020-03-12 10:48:40 +0100

    testing if azure would let me push the commit if u see this then its all good xd

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,7 +1,21 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
 namespace ShinyBooking.Models
 {
     public class Room
     {
-        
+        public int Id { get; set; }\\test
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+        public int OwnerId { get; set; }
+        public List<Equipment> EqList { get; set; }
+        public List<Photo> Photos { get; set; }
+        public string Description { get; set; }
+        public Order Orders { get; set; }
+        public decimal Price { get; set; }
+        public int Area { get; set; }
+        public int Capacity { get; set; }
     }
 }
