25a6637f774220a8f4947f19a376b0bf9d5854be
Bartlomiej Kwoka <52539097+bkwoka@users.noreply.github.com>
2020-03-17 11:45:17 +0100

    AddedConatrainsToRoomTableAndControllersForRoom

---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShinyBooking.Data;
+using ShinyBooking.Models;
+
+namespace ShinyBooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rooms
+        [HttpGet]
+        public async Task<IActionResult> GetRooms()
+        {
+            var rooms = await  _context.Photos.ToListAsync();
+            
+            return Ok(rooms);
+        }
+
+        // GET: api/Rooms/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoom(string id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+
+        // PUT: api/Rooms/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRoom(string id, Room room)
+        {
+            if (id != room.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(room).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rooms
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Room>> PostRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RoomExists(room.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRoom", new { id = room.Id }, room);
+        }
+
+        // DELETE: api/Rooms/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Room>> DeleteRoom(string id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+
+            return room;
+        }
+
+        private bool RoomExists(string id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
+    }
+}
