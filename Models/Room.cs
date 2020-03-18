9aaf6819d5e67937b17a717b956579eb95179cc6
zalewskiwojciech <xiehu43@gmail.com>
2020-03-18 18:14:20 +0100

    Add RoomActivities Models (non DTO) Add Extra Properties to RoomAddress

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShinyBooking.Models
 {
@@ -12,34 +10,37 @@ namespace ShinyBooking.Models
         {
             Id = Guid.NewGuid().ToString();
         }
-        
+
         public string Id { get; set; }
-        
+
         [Required]
-        [StringLength(255, ErrorMessage = "Name is required", MinimumLength = 2)]
+        [StringLength(200, ErrorMessage = "Name is required", MinimumLength = 2)]
         public string Name { get; set; }
-        
+
         [Range(0, 5)]
         public double Rating { get; set; }
-        
+
         [Required]
         [StringLength(255, ErrorMessage = "Description is required", MinimumLength = 2)]
         public string Description { get; set; }
-        
+
         [Required]
-        [Range(0, double.MaxValue, ErrorMessage = " Price must be greater by 0")]
+        [Range(0, double.MaxValue, ErrorMessage = " Price must be greater than 0")]
         public double Price { get; set; }
-        
+
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = " Area must be greater by 0")]
+        [Range(0, int.MaxValue, ErrorMessage = " Area must be greater than 0")]
         public int Area { get; set; }
-        
+
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = " Capacity must be greater by 0")]
+        [Range(0, int.MaxValue, ErrorMessage = " Capacity must be greater than 0")]
         public int Capacity { get; set; }
-        
+
+        [Required]
         public IList<Photo> Photos { get; set; }
 
         public IList<RoomEquipment> RoomEquipments { get; set; }
+
+        public IList<RoomActivities> RoomActivities {get; set;}
     }
 }
---
+++ Models/RoomActivities.cs
@@ -0,0 +1,13 @@
+﻿namespace ShinyBooking.Models
+{
+    public class RoomActivities
+    {
+        public string ActivitiesId { get; set; }
+
+        public Activities Activities{ get; set; }
+
+        public string RoomId { get; set; }
+
+        public Room Room { get; set; }
+    }
+}
