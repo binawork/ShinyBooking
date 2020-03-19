513c97ee45fd2668f8793e50f3c2af7fcf35da86
zalewskiwojciech <xiehu43@gmail.com>
2020-03-19 12:57:35 +0100

    add data annotation

--- Models/Room.cs
+++ Models/Room.cs
@@ -5,7 +5,7 @@ namespace ShinyBooking.Models
 {
     public class Room
     {
-        public int Id { get; set; }\\test
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public double Rating { get; set; }
