6cfaf6d16294d40eca129878134dbd579f162c66
Bartlomiej Kwoka <52539097+bkwoka@users.noreply.github.com>
2020-03-21 01:03:41 +0100

    Update angular, delete unnecessary files and code, cleanup

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ShinyBooking
+namespace shinyBooking3
 {
     public class Program
     {
@@ -18,9 +18,6 @@ namespace ShinyBooking
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
