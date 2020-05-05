bfe5765289464ad1f926698a6c57dfb4b6859feb
zalewskiwojciech <xiehu43@gmail.com>
2020-05-05 14:19:34 +0200

    test commit

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,6 +14,6 @@ namespace ShinyBooking.Models
         public string Locale {get; set;}
         public string Gender {get; set;}
         //test
-        public string Gendera {get; set;}
+       
     }
 }
