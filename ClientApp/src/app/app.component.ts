259d2a7f091480e02feed112e19902a3cf0e7382
JoannaTruszczynska <51478762+JoannaTruszczynska@users.noreply.github.com>
2020-05-13 16:19:44 +0200

    room view refactor

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./styles.css']
 })
 export class AppComponent {
   title = 'MainPage';
