8090e465625bc6227fa72a566765ef21855c0e3b
Karol Karpeta <karol.karpeta@gmail.com>
2020-05-29 10:41:11 +0200

    field token changed to Token in RoomToAddDto

--- ClientApp/src/app/shared/room-to-add-dto.model.ts
+++ ClientApp/src/app/shared/room-to-add-dto.model.ts
@@ -19,7 +19,7 @@ export class RoomToAddDto {
     public roomamenitiesForDisabled: AmenityForDisabled[],
     public roomAddress: RoomAddress,
     public roomactivities: Activity[],
-    public token: string
+    public Token: string
   ) {
     //TODO unhardcode this two fields - user data need to be made for this
     this.roomAddress.emailAddress = 'room@gmail.com';
