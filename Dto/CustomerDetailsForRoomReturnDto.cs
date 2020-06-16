d566233f4806283b599b57cf149ec04044463c21
JoannaTruszczynska <51478762+JoannaTruszczynska@users.noreply.github.com>
2020-06-16 11:01:58 +0200

    UserName added to the customer details in RoomDetails view

--- Dto/CustomerDetailsForRoomReturnDto.cs
+++ Dto/CustomerDetailsForRoomReturnDto.cs
@@ -12,6 +12,7 @@ namespace ShinyBooking.Dto
         public string LastName {get; set;}
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string UserName {get; set;}
 
     }
 }
