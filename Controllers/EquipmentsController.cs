5e799018627da21c115e298f6940a81954ee7707
JoannaTruszczynska <51478762+JoannaTruszczynska@users.noreply.github.com>
2020-06-17 14:46:12 +0200

    equipments api done

---
+++ Controllers/EquipmentsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShinyBooking.Data;
+using ShinyBooking.Dto;
+using ShinyBooking.Models;
+
+namespace ShinyBooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Equipments
+        [HttpGet]
+        //public async Task<ActionResult<IEnumerable<Equipment>>> GetEquipments()
+        //{
+        //    return await _context.Equipments.ToListAsync();
+        //}
+        public async Task<IActionResult> GetEquipments()
+        {
+
+            var equipments = await _context.Equipments.ToListAsync();
+            var equipmentsForReturn = new List<EquipmentForReturnDto>();
+
+            foreach (var equipment in equipments)
+            {
+                var equipmentForReturn = new EquipmentForReturnDto
+
+                {
+                    Id = equipment.Id,
+                    Name = equipment.Name
+                };
+                equipmentsForReturn.Add(equipmentForReturn);
+            }
+            return Ok(equipmentsForReturn);
+
+        }
+
+        // GET: api/Equipments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Equipment>> GetEquipment(string id)
+        {
+            var equipment = await _context.Equipments.FindAsync(id);
+
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return equipment;
+        }
+
+        // PUT: api/Equipments/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEquipment(string id, Equipment equipment)
+        {
+            if (id != equipment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(equipment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Equipments
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Equipment>> PostEquipment(Equipment equipment)
+        {
+            _context.Equipments.Add(equipment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EquipmentExists(equipment.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEquipment", new { id = equipment.Id }, equipment);
+        }
+
+        // DELETE: api/Equipments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Equipment>> DeleteEquipment(string id)
+        {
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Equipments.Remove(equipment);
+            await _context.SaveChangesAsync();
+
+            return equipment;
+        }
+
+        private bool EquipmentExists(string id)
+        {
+            return _context.Equipments.Any(e => e.Id == id);
+        }
+    }
+}
